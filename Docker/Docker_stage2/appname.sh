81922b5789ef385ba132889585001471703830cb
root <root@ip-172-31-81-241.ec2.internal>
2025-06-26 11:52:17 +0000

    new dockerfiles intermediate level

---
+++ Docker/Docker_stage2/appname.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+
+echo "App Name:$APPNAME"
+echo "App Version: $APPVERSION"
+
---
+++ Docker/Docker_stage2/checking.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+echo "Hey this container is created in a secure way using the user: $USERNAME"
